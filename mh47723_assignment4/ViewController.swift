9108b47db14260d6160ca04afd9b7501534917d9
Anonymous85 <anonymous85@Anonymous85s-MacBook-Pro.local>
2016-09-18 09:55:39 -0500

    Initial Commit

---
+++ mh47723_assignment4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mh47723_assignment4
+//
+//  Created by Anonymous85 on 9/18/16.
+//  Copyright © 2016 Minh-Tri Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
